f691a33f477070c5a559e8b70d49d235bd722ceb
Louis Jacob <loujacob2@gmail.com>
2021-10-18 17:01:59 -0400

    commit tree 6fd4baf2335b943911d3f0ab443b65befac68ced

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const hq = 42
+
+function distanceFromHqInBlocks(location){
+    return Math.abs (location - hq); 
+}
+
+function distanceFromHqInFeet(location) {
+    distanceFromHqInBlocks(location);
+    return (Math.abs (location - hq) * 264);
+}
+
+function distanceTravelledInFeet (hq, location){
+    distanceFromHqInBlocks(location);
+    return (Math.abs (location - hq) * 264);
+}
+
+const calculatesFarePrice = (sBlock, eBlock) => {
+    let ride = distanceTravelledInFeet(sBlock, eBlock);
+
+    if (ride <= 400) {
+        return 0;
+    } else if (ride <= 2000) {
+        return (ride - 400) * 0.02;
+    } else if (ride <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
+
